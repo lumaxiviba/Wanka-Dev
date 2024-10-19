7159d3c32192cefec1a43ee6c1f1a88c2a764532
Lucciano Maximo <luccianobrzl@gmail.com>
2024-10-19 14:51:14 -0500

    Agregando contenido a la sección de noticias y eventos.

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ ScrollReveal().reveal(".hero-image");
 ScrollReveal().reveal(".news-cards", { delay: 500 });
 ScrollReveal().reveal(".cards-banner-one", { delay: 500 });
 ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
+
+ScrollReveal().reveal(".box-article", { delay: 500 });
+ScrollReveal().reveal(".cards-banner-one", { delay: 500 });
+ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
