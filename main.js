98ef476d7e56a3d970bfb1e70af60ef46acf1a10
Lucciano Maximo <luccianobrzl@gmail.com>
2024-10-19 20:00:30 -0500

    Terminando de agregar contenido a la página web.

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
   document.querySelector(".nav-menu").classList.toggle("show");
 });
 
-ScrollReveal().reveal(".hero-image");
-ScrollReveal().reveal(".news-cards", { delay: 500 });
-ScrollReveal().reveal(".cards-banner-one", { delay: 500 });
-ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
+ScrollReveal().reveal(".hero-image", { delay: 100 });
+ScrollReveal().reveal(".news-cards", { delay: 300 });
+ScrollReveal().reveal(".cards-banner-one", { delay: 300 });
+ScrollReveal().reveal(".cards-banner-two", { delay: 300 });
 
-ScrollReveal().reveal(".box-article", { delay: 500 });
-ScrollReveal().reveal(".cards-banner-one", { delay: 500 });
-ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
+ScrollReveal().reveal(".box-article", { delay: 300 });
+ScrollReveal().reveal(".cards-banner-one", { delay: 300 });
+ScrollReveal().reveal(".cards-banner-two", { delay: 300 });
+ScrollReveal().reveal(".fas", { delay: 300 });
