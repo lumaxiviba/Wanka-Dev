c8e6c99eaf5674b25109f932759c7641899eeb94
Lucciano Maximo <luccianobrzl@gmail.com>
2024-10-27 21:24:49 -0500

    Diseñando mi barra de navegación:

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+//Codigo para el Responsive Design
 document.querySelector(".menu-btn").addEventListener("click", () => {
   document.querySelector(".nav-menu").classList.toggle("show");
 });
-
+//Realizando animaciones
 ScrollReveal().reveal(".hero-image", { delay: 100 });
 ScrollReveal().reveal(".news-cards", { delay: 300 });
 ScrollReveal().reveal(".cards-banner-one", { delay: 300 });
@@ -11,3 +12,33 @@ ScrollReveal().reveal(".box-article", { delay: 300 });
 ScrollReveal().reveal(".cards-banner-one", { delay: 300 });
 ScrollReveal().reveal(".cards-banner-two", { delay: 300 });
 ScrollReveal().reveal(".fas", { delay: 300 });
+
+// BUSCADOR DE CONTENIDO
+
+// Ejecutando las funciones
+document
+  .getElementById("icon-search")
+  .addEventListener("click", mostrarBuscador);
+document
+  .getElementById("cover-ctn-search")
+  .addEventListener("click", ocultar_buscador);
+
+//Declarando mis variables
+let bars_search = document.getElementById("ctn-bars-search");
+let cover_ctn_search = document.getElementById("cover-ctn-search");
+let input_search = document.getElementById("inputSearch");
+let box_search = document.getElementById("box-search");
+
+//Funcion para mostrar el buscador
+function mostrarBuscador() {
+  bars_search.style.top = "80px";
+  cover_ctn_search.style.display = "block";
+  input_search.focus();
+}
+
+//Función para ocultar el buscador
+function ocultar_buscador() {
+  bars_search.style.top = "-100px";
+  cover_ctn_search.style.display = "none";
+  input_search.value = "";
+}
